36f3917ac4fc3b6b2b576a32440a5d6f385afb02
Ninon <157741834+NinonCrst@users.noreply.github.com>
2025-03-21 16:41:37 +0100

    Add files via upload

---
+++ Hotel.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import Lieu_trsm;
+
+public class Hotel{
+    String pays_h;
+    String ville_h;
+    String ad_p_h;
+    String nm_h;
+    String num_t_h;
+    String ad_m_h;
+    boolean piscine;
+    Lieu_trsm[] lieux_prx; 
+
+    public Hotel(String pays, String ville, String ad_p, nm, String num, String ad_m, Lieu_trsm[] prx){
+        pays_h = pays;
+        ville_h = ville;
+        ad_p_h = ad_p;
+        nm_h = nm;
+        num_t_h = num;
+        ad_m_h = ad_m;
+        lieux_prx = prx;
+        piscine = false;
+    }
+
+    public Hotel(String pays, String ville, String ad_p, nm, String num, String ad_m, Lieu_trsm[] prx, boolean pisc){
+        pays_h = pays;
+        ville_h = ville;
+        ad_p_h = ad_p;
+        nm_h = nm;
+        num_t_h = num;
+        ad_m_h = ad_m;
+        lieux_prx = prx;
+        piscine = pisc;
+    }
+}
