83bfe3fb17693f057e54636831ee29ede100ec85
Ameylia clement <159237041+Ameyliaclt@users.noreply.github.com>
2025-03-22 00:39:33 +0100

    Update main.java

--- main.java
+++ main.java
@@ -13,7 +13,22 @@ public static Lieu_trsm[] Lieu_p(){
 
     return lieux_pa;
 }
+public static Lieu_trsm[] Lieu_b(){
+    Lieu_trsm[] lieux_ba = new Lieu_trsm[10];
 
+    lieux_ba[0] l_b_m = new Lieu_trsm("Wat Pho(วัดโพธิ์)","2 Sanamchai Road, Grand Palace Subdistrict, Pranakorn District, Bangkok 10200, Thaïlande","+66830577100");
+    lieux_ba[1] l_b_c = new Lieu_trsm("The Grand Palace(พระบรมมหาราชวัง)","Na Phra Lan Rd, Phra Nakhon, Bangkok 10200, Thaïlande","+66232259519");
+    lieux_ba[2] l_b_d = new Lieu_trsm("Bangkok Yai Park(สวนบางกอกใหญ่)","PFJH+J8Q, Wat Tha Phra, Bangkok Yai, Bangkok 10600, Thaïlande","+6624655640");
+    lieux_ba[3] l_b_cm = new Lieu_trsm("CentralWorld(เซ็นทรัลเวิลด์)","999/9 Rama I Rd, Pathum Wan, Bangkok 10330, Thaïlande","+6626407000");
+    lieux_ba[4] l_b_pa = new Lieu_trsm("Siam Amazing Park(สยามอะเมซิ่งพาร์ค)","203 Suan Siam Rd, Khwaeng Khan Na Yao, Khet Khan Na Yao, Krung Thep Maha Nakhon 10230, Thaïlande","+6621054292");
+    lieux_ba[5] l_b_rt = new Lieu_trsm("Kinu by Tagaki","Mandarin Oriental Bangkok, 48 Oriental Ave, Khwaeng Bang Rak, Bang Rak, Bangkok 10500, Thaïlande","+6626599000");
+    lieux_ba[6] l_b_aprt = new Lieu_trsm("Suvarnabhumi Airport (BKK)","999, Nong Prue, Bang Phli District, Samut Prakan 10540, Thaïlande","+6621321888");
+    lieux_ba[7] l_b_gr = new Lieu_trsm("Bangkok Railway Station(กรุงเทพ)","Rong Mueang Rd, Rong Muang, Pathum Wan, Bangkok 10330, Thaïlande","+6622204334");
+    lieux_ba[8] l_b_prt = new Lieu_trsm("Bangkok Port(ท่าเรือกรุงเทพ)","Khlong Toei, Bangkok 10110, Thaïlande","+6622384000");
+    lieux_ba[9] l_b_lcv = new Lieu_trsm("Drive Car Rental - Suvarnabhumi Airport","Suvarnabhumi Airport,, Arrival Hall, 999 Moo 1, Nong Prue,, Bang Phli District, Samut Prakan 10540, Thaïlande","+66847004376");
+
+    return lieux_ba;
+}
 public static Lieu_trsm[] lieux_k(){
     Lieu_trsm[] lieux_ky = new Lieu_trsm[10];
     
@@ -52,4 +67,6 @@ public static void main(String[] args){
     Hotel h_p = new Hotel("France", "Paris","127 Av. des Champs-Élysées, 75008 Paris","élégance","0145678926","elegance_paris@hotel.fr",Lieu_p());
     Hotel h_k = new Hotel("Japon", "Kyoto", "", "Atarashii Kyoto", "atarashiiKyoto@hotel.com", Lieux_k());
     Hotel h_r = new Hotel("Islande", "Reykjavik", "", "", "", Lieux_r());
+    Hotel h_b = new Hotel("Thailande","Bangkok", "PFWR+C85, Maha Rat Rd, Phra Borom Maha Ratchawang, Phra Nakhon, Bangkok 10200, Thaïlande","Wat Pho(วัดโพธิ์) Hotel","+66 89 057 7100","watpho_hotel@hotel.com",Lieu_b());
+
 }
