1cf8a3cbea677bfe3fc8b94d0f310057adecb7a1
Ninon <157741834+NinonCrst@users.noreply.github.com>
2025-05-11 23:15:56 +0200

    Update Reservation.java
    Modification pour la vue gestion et ajout des bons mots-clé

--- Reservation.java
+++ Reservation.java
@@ -1,39 +1,35 @@
 import java.util.*;
-import Client;
-import Chambre; 
-import Hotel; 
-import Date; 
 
 public class Reservation{
-    String[] stat = {"annule", "confirme"}
+    private String[] stat = {"annule", "confirme", "attente"};
 
-    Date date_r_d;
-    Date date_r_f;
-    Client client;
-    Hotel hotel;
-    float prix_r;
-    Chambre[] chmb_r;
-    String statut_r;
+    public Date date_r_d;
+    public Date date_r_f;
+    public Client client;
+    public Hotel hotel;
+    public float prix_r;
+    public Chambre[] chmb_r;
+    public String statut_r;
 
-    public Reservation(Date date_d, Date date_f, Client clt, Hotel htl, float prix, Chambre[] chmb){
+    public Reservation(Date date_d, Date date_f, Client clt, Hotel htl, float prix, Chambre[] chmb, int stt){
         date_r_d = date_d;
         date_r_f = date_f;
         client = clt;
         hotel = htl;
         prix_r = prix;
         chmb_r = chmb;
-        statut_r = " ";
+        statut_r = stat[stt];
     }
 
-    void add_r(Reservation r){
+    public void add_r(Reservation r){
         statut_r = stat[1];
     }
 
-    void rmv_r(Reservation r){
+    public void rmv_r(Reservation r){
         statut_r = stat[0];
     }
 
-    void modif_date_r(Reservation r, Date deb, Date fin){
+    public void modif_date_r(Reservation r, Date deb, Date fin){
         date_r_d = deb;
         date_r_f = fin;
     }
