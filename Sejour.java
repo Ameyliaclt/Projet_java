7dc2a9d3eb9fb29a68cde900b81cd65d5c323a7f
Ninon <157741834+NinonCrst@users.noreply.github.com>
2025-05-11 23:20:31 +0200

    Update Sejour.java
    Suppression des indentations étranges

--- Sejour.java
+++ Sejour.java
@@ -6,12 +6,12 @@ public class Sejour{
 	public Conso_mb[] consos;
 	private int index = 0;
 
-    public Sejour(Reservation rsrvt) {
-         this.rsrvt = rsrvt;
-		 this.consos = new Conso_mb[0]; 
-    }  
+        public Sejour(Reservation rsrvt) {
+        	this.rsrvt = rsrvt;
+		this.consos = new Conso_mb[0]; 
+    	}  
 
-    public void add_conso_mb(Conso_mb conso) {
+    	public void add_conso_mb(Conso_mb conso) {
 		if(index >= consos.length){
 			Conso_mb[] newC = new Conso_mb[consos.length+1];
 			System.arraycopy(consos, 0, newC, 0, consos.length);
@@ -19,10 +19,10 @@ public class Sejour{
 		}
 		consos[index] = conso;
 		index++;
-    }
+    	}
 
-    public float fct() { 
-        float prix = 0;
+    	public float fct() { 
+        	float prix = 0;
 		for(int i = 0; i<rsrvt.chmb_r.length; i++){
 			prix += rsrvt.chmb_r[i].prix_ch;
 		}
@@ -30,5 +30,5 @@ public class Sejour{
 			prix += consos[i].prix_mb;	
 		}
 		return prix;
-    } 
+    	} 
 }
