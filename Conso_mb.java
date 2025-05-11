e477313f66c04b26441fa2a9f3cf3d78396c1eb3
Ninon <157741834+NinonCrst@users.noreply.github.com>
2025-05-11 23:00:29 +0200

    Update Conso_mb.java
    Modification de la classe Conso_mb en supprimant les fonctions inutiles et en mettant les bons mots-clé

--- Conso_mb.java
+++ Conso_mb.java
@@ -1,20 +1,11 @@
 import java.util.*;
 
 public class Conso_mb{
-	String t_mb;
-	float prix_mb;
+	public String t_mb;
+	public float prix_mb;
 
 	public Conso_mb(String t , float prix){
 		t_mb = t; 
 		prix_mb = prix;
- 	}
-
- 	public String getType (){ 
-    		return t_mb;
- 	} 
-
- 	public float getPrix (){
-   		return prix_mb;
-
  	}
 }
