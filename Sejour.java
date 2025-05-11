e095f13f1249a0890457b99e35e28f2d49269ceb
Ninon <157741834+NinonCrst@users.noreply.github.com>
2025-05-11 23:18:54 +0200

    Update Sejour.java
    Modification de la classe et ajout des bons mots-clé

--- Sejour.java
+++ Sejour.java
@@ -1,35 +1,34 @@
-impot java.util.*;
+import java.util.*;
+import java.lang.*;
 
 public class Sejour{
-    Reservation rsrvt;
-    public Sejour() {
-         this.rsrvt = null;
-          }
+	public Reservation rsrvt;
+	public Conso_mb[] consos;
+	private int index = 0;
 
     public Sejour(Reservation rsrvt) {
-         this.rsrvt = rsrvt; 
-         }
+         this.rsrvt = rsrvt;
+		 this.consos = new Conso_mb[0]; 
+    }  
 
-    public Reservation getRsrvt() {
-         return rsrvt; 
-         } 
-
-         public void setRsrvt(Reservation rsrvt) { 
-        this.rsrvt = rsrvt; 
-        }
-
-    public Sejour add_sej() { 
-        return new Sejour();
-        } 
-
-    public Conso_mb add_conso_mb() { 
-        return new Conso_mb();
-        }
+    public void add_conso_mb(Conso_mb conso) {
+		if(index >= consos.length){
+			Conso_mb[] newC = new Conso_mb[consos.length+1];
+			System.arraycopy(consos, 0, newC, 0, consos.length);
+			consos = newC;
+		}
+		consos[index] = conso;
+		index++;
+    }
 
     public float fct() { 
-        return 0.0 ; 
-        } 
+        float prix = 0;
+		for(int i = 0; i<rsrvt.chmb_r.length; i++){
+			prix += rsrvt.chmb_r[i].prix_ch;
+		}
+		for(int i = 0; i<consos.length; i++){
+			prix += consos[i].prix_mb;	
+		}
+		return prix;
+    } 
 }
-        
-
-
