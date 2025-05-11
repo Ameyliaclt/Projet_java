66b16d9cd83ce0c15028f5471c5de2bc3ba726b7
Ninon <157741834+NinonCrst@users.noreply.github.com>
2025-05-11 22:59:34 +0200

    Update Client.java
    Suppression des variables inutiles et du constructeur inutile

--- Client.java
+++ Client.java
@@ -1,31 +1,25 @@
 import java.util.*;
-public class Client {
-    private String nm_cl;
-    private String pnm_cl;
-    private String num_t_cl;
-    private String ad_m_cl;
-    private String ad_p_cl;
-    private String cd_b_cl;
-	private String psw;
 
-    public Client(String nm, String pnm, String num_t, String ad_m, String ad_p, String cd_b){
+public class Client {
+    public String nm_cl;
+    public String pnm_cl;
+    public String num_t_cl;
+    public String ad_m_cl;
+    public String psw;
+
+    public Client(String nm, String pnm, String num_t, String ad_m, String password){
         this.nm_cl = nm;
         this.pnm_cl = pnm;
         this.num_t_cl = num_t;
         this.ad_m_cl = ad_m;
-        this.ad_p_cl = ad_p;
-        this.cd_b_cl = cd_b;
-    }
-	
-	public Client(String nom, String prenom, String phone, String email, String password) {
-        this.nm_cl = nom;
-        this.pnm_cl = prenom;
-        this.num_t_cl = phone;
-        this.ad_m_cl = email;
-		this.psw = password;
+        this.psw = password;
     }
 
     public String getPassword() {
         return psw;
+    }
+	
+    public String getPnm() {
+	return pnm_cl;
     }
 }
