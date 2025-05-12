70531dfe505d805132c0d8bedc4f97cd09ead86c
Ninon <157741834+NinonCrst@users.noreply.github.com>
2025-05-13 01:03:01 +0200

    Update Reservation.java
    Modification du changement de statut

--- Reservation.java
+++ Reservation.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Reservation{
-    private String[] stat = {"annule", "confirme", "attente"};
+    protected String[] stat = {"annule", "confirme", "attente"};
 
     public Date date_r_d;
     public Date date_r_f;
@@ -12,25 +12,29 @@ public class Reservation{
     public String statut_r;
 
     public Reservation(Date date_d, Date date_f, Client clt, Hotel htl, float prix, Chambre[] chmb, int stt){
-        date_r_d = date_d;
-        date_r_f = date_f;
-        client = clt;
-        hotel = htl;
-        prix_r = prix;
-        chmb_r = chmb;
-        statut_r = stat[stt];
+        this.date_r_d = date_d;
+        this.date_r_f = date_f;
+        this.client = clt;
+        this.hotel = htl;
+        this.prix_r = prix;
+        this.chmb_r = chmb;
+        this.statut_r = stat[stt];
     }
 
-    public void add_r(Reservation r){
-        statut_r = stat[1];
+    public Sejour add_r(){
+	ArrayUtils au1 = new ArrayUtils();
+	int stt = au1.indexOf(this.stat, this.statut_r);
+        this.statut_r = stat[1];
+	Sejour sej = new Sejour(this, stt);
+	return sej;
     }
 
-    public void rmv_r(Reservation r){
-        statut_r = stat[0];
+    public void rmv_r(){
+        this.statut_r = stat[0];
     }
 
-    public void modif_date_r(Reservation r, Date deb, Date fin){
-        date_r_d = deb;
-        date_r_f = fin;
+    public void modif_date_r(Date deb, Date fin){
+        this.date_r_d = deb;
+        this.date_r_f = fin;
     }
 }
