d94225c0c3486bb767c5b8a3df091e76b515b167
Ninon <157741834+NinonCrst@users.noreply.github.com>
2025-05-13 01:04:03 +0200

    Update Sejour.java
    Correction de bugs et modification du constructeur et de sa place dans l'héritage

--- Sejour.java
+++ Sejour.java
@@ -1,15 +1,14 @@
 import java.util.*;
 import java.lang.*;
 
-public class Sejour{
-	public Reservation rsrvt;
+public class Sejour extends Reservation{
 	public Conso_mb[] consos;
 	private int index = 0;
 
-        public Sejour(Reservation rsrvt) {
-        	this.rsrvt = rsrvt;
+    	public Sejour(Reservation rsrv, int stt) {
+         	super(rsrv.date_r_d, rsrv.date_r_f, rsrv.client, rsrv.hotel, rsrv.prix_r, rsrv.chmb_r, stt);
 		this.consos = new Conso_mb[0]; 
-    	}  
+        }  
 
     	public void add_conso_mb(Conso_mb conso) {
 		if(index >= consos.length){
@@ -19,16 +18,13 @@ public class Sejour{
 		}
 		consos[index] = conso;
 		index++;
-    	}
+        }
 
     	public float fct() { 
-        	float prix = 0;
-		for(int i = 0; i<rsrvt.chmb_r.length; i++){
-			prix += rsrvt.chmb_r[i].prix_ch;
-		}
+        	float prix = this.prix_r;
 		for(int i = 0; i<consos.length; i++){
 			prix += consos[i].prix_mb;	
 		}
 		return prix;
-    	} 
+        } 
 }
